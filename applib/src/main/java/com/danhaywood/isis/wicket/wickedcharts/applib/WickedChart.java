812f4864c55c5fbe808897afca54a7f02cf26868
Dan Haywood <dan@haywood-associates.co.uk>
2013-02-16 10:38:03 +0000

    Added title() to WickedChart

--- applib/src/main/java/com/danhaywood/isis/wicket/wickedcharts/applib/WickedChart.java
+++ applib/src/main/java/com/danhaywood/isis/wicket/wickedcharts/applib/WickedChart.java
@@ -6,6 +6,7 @@ import org.apache.isis.applib.annotation.Programmatic;
 import org.apache.isis.applib.annotation.Value;
 
 import com.googlecode.wickedcharts.highcharts.options.Options;
+import com.googlecode.wickedcharts.highcharts.options.Title;
 
 @Value(semanticsProviderClass=WickedChartSemanticsProvider.class)
 public class WickedChart implements Serializable {
@@ -18,6 +19,11 @@ public class WickedChart implements Serializable {
         this.options = options;
     }
 
+    public String title() {
+        Title title = getOptions().getTitle();
+        return title != null? title.getText(): "Wicked Chart";
+    }
+    
     @Programmatic
     public Options getOptions() {
         return options;
