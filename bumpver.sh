71871ee676890ada2acd59e2d8cd9c88e7f69f81
Dan Haywood <dan@haywood-associates.co.uk>
2014-09-29 19:59:49 +0100

    fix in bumpver.sh

--- bumpver.sh
+++ bumpver.sh
@@ -6,13 +6,13 @@ if [ ! "$VERSION" ]; then
 fi
 
 # edit parent pom.xml's reference
-echo "editing parent's pom.xml (reference to dom module)"
+echo "editing parent's pom.xml (reference to cpt module)"
 cat pom.xml | sed "s/<isis-wicket-excel.version>.*</<isis-wicket-excel.version>$VERSION</" > pom.xml.$$.sed
 mv pom.xml.$$.sed pom.xml
 
-# edit dom's pom.xml
-echo "editing dom's pom.xml"
-pushd dom >/dev/null
+# edit cpt's pom.xml
+echo "editing cpt's pom.xml"
+pushd cpt >/dev/null
 mvn versions:set -DnewVersion=$VERSION > /dev/null
 popd >/dev/null
 
