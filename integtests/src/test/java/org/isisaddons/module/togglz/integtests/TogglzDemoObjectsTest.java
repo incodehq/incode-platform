0fc98beaf1d6be89cb2a56c66c6ca148668a9fbe
Dan Haywood <dan@haywood-associates.co.uk>
2015-05-08 18:00:36 +0100

    initial take-on of template (converted xxx -> togglz); also added dependencies to togglz in pom.xml.  No other changes.

---
+++ integtests/src/test/java/org/isisaddons/module/togglz/integtests/TogglzDemoObjectsTest.java
@@ -0,0 +1,67 @@
+/*
+ *  Copyright 2014~2015 Dan Haywood
+ *
+ *  Licensed under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.isisaddons.module.togglz.integtests;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.apache.isis.applib.fixturescripts.FixtureScripts;
+
+import org.isisaddons.module.togglz.fixture.dom.TogglzDemoObject;
+import org.isisaddons.module.togglz.fixture.dom.TogglzDemoObjects;
+import org.isisaddons.module.togglz.fixture.scripts.scenarios.TogglzDemoObjectsFixture;
+
+
+public class TogglzDemoObjectsTest extends TogglzModuleIntegTest {
+
+    @Inject
+    FixtureScripts fixtureScripts;
+
+    @Inject
+    private TogglzDemoObjects togglzDemoObjects;
+
+    @Before
+    public void setUpData() throws Exception {
+        fixtureScripts.runFixtureScript(new TogglzDemoObjectsFixture(), null);
+    }
+
+
+    @Test
+    public void listAll() throws Exception {
+
+        final List<TogglzDemoObject> all = wrap(togglzDemoObjects).listAll();
+        Assertions.assertThat(all.size()).isEqualTo(3);
+        
+        TogglzDemoObject togglzDemoObject = wrap(all.get(0));
+        Assertions.assertThat(togglzDemoObject.getName()).isEqualTo("Foo");
+    }
+    
+    @Test
+    public void create() throws Exception {
+
+        wrap(togglzDemoObjects).create("Faz");
+        
+        final List<TogglzDemoObject> all = wrap(togglzDemoObjects).listAll();
+        Assertions.assertThat(all.size()).isEqualTo(4);
+    }
+
+}
