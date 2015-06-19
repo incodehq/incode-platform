62f4b78c914e5ff3576941f4178eb776a32585ab
Dan Haywood <dan@haywood-associates.co.uk>
2015-06-19 16:30:44 +0100

    working on cxf stuff, getting bootstrapped into demo app.

---
+++ soapsubscriber-wsdl/src/main/java/org/isisaddons/module/publishmq/soapsubscriber/demoobject/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://isisaddons.org/module/publishmq/soapsubscriber/DemoObject/")
+package org.isisaddons.module.publishmq.soapsubscriber.demoobject;
