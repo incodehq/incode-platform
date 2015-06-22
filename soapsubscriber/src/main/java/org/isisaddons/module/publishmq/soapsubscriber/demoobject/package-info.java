a271fa78de1ca820611174dab7c467109ed599e1
Dan Haywood <dan@haywood-associates.co.uk>
2015-06-22 23:22:12 +0100

    ok, have the SOAP call working (fiddling with versions of CXF jar files)

--- soapsubscriber/src/main/java/org/isisaddons/module/publishmq/soapsubscriber/demoobject/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://isisaddons.org/module/publishmq/soapsubscriber/DemoObject/")
-package org.isisaddons.module.publishmq.soapsubscriber.demoobject;
