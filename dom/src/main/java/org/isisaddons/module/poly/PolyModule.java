1eaa89dce84e5281d9c9ba3b83b0f6c62f3e12d3
Dan Haywood <dan@haywood-associates.co.uk>
2015-08-13 14:35:14 +0100

    moving PolyModule to correct package

--- dom/src/main/java/org/isisaddons/module/poly/PolyModule.java
+++ dom/src/main/java/org/isisaddons/module/poly/PolyModule.java
@@ -14,7 +14,7 @@
  *  specific language governing permissions and limitations
  *  under the License.
  */
-package org.isisaddons.module.poly.dom;
+package org.isisaddons.module.poly;
 
 import java.util.List;
 import org.apache.isis.applib.Identifier;
