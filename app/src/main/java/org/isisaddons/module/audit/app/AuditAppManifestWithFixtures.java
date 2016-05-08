693f4959b7c9c55950de56a6c55887beae91fd38
Dan Haywood <dan@haywood-associates.co.uk>
2016-05-08 17:38:57 +0100

    adding an AppManifest for fixtures

--- app/src/main/java/org/isisaddons/module/audit/app/AuditAppManifestWithFixtures.java
+++ app/src/main/java/org/isisaddons/module/audit/app/AuditAppManifestWithFixtures.java
@@ -20,30 +20,25 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.isis.applib.AppManifest;
+import com.google.common.collect.Maps;
+
 import org.apache.isis.applib.fixturescripts.FixtureScript;
 
-import org.isisaddons.module.audit.AuditModule;
-import org.isisaddons.module.audit.fixture.AuditFixtureModule;
+import org.isisaddons.module.audit.fixture.scripts.SomeAuditedObjectsFixture;
 
-public class AuditAppManifest implements AppManifest {
+public class AuditAppManifestWithFixtures extends AuditAppManifest {
+
     @Override
-    public List<Class<?>> getModules() {
-        return Arrays.asList(
-                AuditModule.class,
-                AuditFixtureModule.class,
-                AuditAppModule.class
-        );
+    public List<Class<? extends FixtureScript>> getFixtures() {
+        return Arrays.<Class<? extends FixtureScript>>asList(
+                SomeAuditedObjectsFixture.class);
+
     }
     @Override
-    public List<Class<?>> getAdditionalServices() { return null; }
-    @Override
-    public String getAuthenticationMechanism() { return null; }
-    @Override
-    public String getAuthorizationMechanism() { return null; }
-    @Override
-    public List<Class<? extends FixtureScript>> getFixtures() { return null; }
-    @Override
-    public Map<String, String> getConfigurationProperties() { return null; }
+    public Map<String, String> getConfigurationProperties() {
+        final Map<String, String> props = Maps.newHashMap();
+        props.put("isis.persistor.datanucleus.install-fixtures", "true");
+        return props;
+    }
 
 }
