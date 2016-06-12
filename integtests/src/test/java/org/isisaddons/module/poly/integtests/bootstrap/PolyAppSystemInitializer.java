b93161075c79b8da1ad29b4fff490c560c422501
Dan Haywood <dan@haywood-associates.co.uk>
2016-06-12 18:35:25 +0100

    required updates to integ tests as per ISIS-1427 refactorings

--- integtests/src/test/java/org/isisaddons/module/poly/integtests/bootstrap/PolyAppSystemInitializer.java
+++ integtests/src/test/java/org/isisaddons/module/poly/integtests/bootstrap/PolyAppSystemInitializer.java
@@ -18,38 +18,59 @@
  */
 package org.isisaddons.module.poly.integtests.bootstrap;
 
-import org.apache.isis.core.commons.config.IsisConfiguration;
+import com.google.common.collect.Lists;
+import org.apache.isis.applib.AppManifest;
+import org.apache.isis.applib.fixturescripts.FixtureScript;
 import org.apache.isis.core.integtestsupport.IntegrationTestAbstract;
 import org.apache.isis.core.integtestsupport.IsisSystemForTest;
-import org.apache.isis.objectstore.jdo.datanucleus.DataNucleusPersistenceMechanismInstaller;
 import org.apache.isis.objectstore.jdo.datanucleus.IsisConfigurationForJdoIntegTests;
+import org.isisaddons.module.poly.PolyModule;
+
+import java.util.List;
+import java.util.Map;
 
 public class PolyAppSystemInitializer extends IntegrationTestAbstract {
 
     public static void initIsft() {
         IsisSystemForTest isft = IsisSystemForTest.getElseNull();
         if(isft == null) {
-            isft = new SimpleAppSystemBuilder().build().setUpSystem();
+            isft = new IsisSystemForTest.Builder()
+                    .withLoggingAt(org.apache.log4j.Level.INFO)
+                    .with(new AppManifest() {
+                        @Override
+                        public List<Class<?>> getModules() {
+                            return Lists.<Class<?>>newArrayList(PolyModule.class);
+                        }
+
+                        @Override
+                        public List<Class<?>> getAdditionalServices() {
+                            return null;
+                        }
+
+                        @Override
+                        public String getAuthenticationMechanism() {
+                            return null;
+                        }
+
+                        @Override
+                        public String getAuthorizationMechanism() {
+                            return null;
+                        }
+
+                        @Override
+                        public List<Class<? extends FixtureScript>> getFixtures() {
+                            return null;
+                        }
+
+                        @Override
+                        public Map<String, String> getConfigurationProperties() {
+                            return null;
+                        }
+                    })
+                    .with(new IsisConfigurationForJdoIntegTests())
+                    .build()
+                    .setUpSystem();
             IsisSystemForTest.set(isft);
-        }
-    }
-
-    private static class SimpleAppSystemBuilder extends IsisSystemForTest.Builder {
-
-        public SimpleAppSystemBuilder() {
-            withLoggingAt(org.apache.log4j.Level.INFO);
-            with(testConfiguration());
-            with(new DataNucleusPersistenceMechanismInstaller());
-
-            // services annotated with @DomainService
-            withServicesIn( "org.isisaddons.module.poly" );
-        }
-
-        private static IsisConfiguration testConfiguration() {
-            final IsisConfigurationForJdoIntegTests testConfiguration = new IsisConfigurationForJdoIntegTests();
-
-            testConfiguration.addRegisterEntitiesPackagePrefix("org.isisaddons.module.poly");
-            return testConfiguration;
         }
     }
 }
