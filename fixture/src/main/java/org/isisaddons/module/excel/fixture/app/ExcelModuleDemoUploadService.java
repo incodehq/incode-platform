8230fb127e465aca9dde4a3ac12060528e6c0d89
Dan Haywood <dan@haywood-associates.co.uk>
2016-08-30 16:28:14 +0100

    moves the demo upload service (showcasing ExcelFixture) to reside under Prototyping menu.

--- fixture/src/main/java/org/isisaddons/module/excel/fixture/app/ExcelModuleDemoUploadService.java
+++ fixture/src/main/java/org/isisaddons/module/excel/fixture/app/ExcelModuleDemoUploadService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.inject.Inject;
 
 import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.DomainServiceLayout;
 import org.apache.isis.applib.annotation.NatureOfService;
 import org.apache.isis.applib.annotation.Optionality;
 import org.apache.isis.applib.annotation.Parameter;
@@ -18,13 +19,14 @@ import org.isisaddons.module.excel.dom.ExcelFixture;
 import org.isisaddons.module.excel.fixture.scripts.ExcelModuleDemoToDoItemRowHandler;
 import org.isisaddons.module.excel.fixture.viewodels.ExcelModuleDemoProfessionalImp;
 
-@DomainService(nature = NatureOfService.VIEW)
+@DomainService(nature = NatureOfService.VIEW_MENU_ONLY)
+@DomainServiceLayout(menuBar = DomainServiceLayout.MenuBar.SECONDARY, named = "Prototyping", menuOrder = "900")
 public class ExcelModuleDemoUploadService {
 
     public List<FixtureResult> uploadSpreadsheet(
-            @ParameterLayout(named = "file")
+            @ParameterLayout(named = "spreadsheet")
             final Blob file,
-            @ParameterLayout(named = "parameters")
+            @ParameterLayout(named = "ExcelFixture parameters")
             @Parameter(optionality = Optionality.OPTIONAL)
             final String parameters){
         FixtureScript script = new ExcelFixture(
