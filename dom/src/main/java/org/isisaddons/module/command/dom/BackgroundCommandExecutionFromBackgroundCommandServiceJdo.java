c56b320d78382056a13ac2509178458b0a08aa5a
Dan Haywood <dan@haywood-associates.co.uk>
2016-09-27 17:19:22 +0100

    fixes #10 - workaround for ISIS-1497.

--- dom/src/main/java/org/isisaddons/module/command/dom/BackgroundCommandExecutionFromBackgroundCommandServiceJdo.java
+++ dom/src/main/java/org/isisaddons/module/command/dom/BackgroundCommandExecutionFromBackgroundCommandServiceJdo.java
@@ -21,8 +21,12 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.apache.isis.applib.services.bookmark.Bookmark;
 import org.apache.isis.applib.services.command.Command;
+import org.apache.isis.core.metamodel.adapter.ObjectAdapter;
+import org.apache.isis.core.metamodel.adapter.oid.RootOid;
 import org.apache.isis.core.runtime.services.background.BackgroundCommandExecution;
+import org.apache.isis.schema.common.v1.OidDto;
 
 /**
  * If used, ensure that <code>org.apache.isis.module:isis-module-background</code> is also included on classpath.
@@ -38,7 +42,23 @@ public final class BackgroundCommandExecutionFromBackgroundCommandServiceJdo ext
         LOG.debug("Found " + commands.size() + " to execute");
         return commands; 
     }
-    
+
+    /**
+     * TODO: workaround for ISIS-1497.
+     */
+    @Override
+    protected ObjectAdapter adapterFor(final Object targetObject) {
+        if(targetObject instanceof OidDto) {
+            final OidDto oidDto = (OidDto) targetObject;
+            final Bookmark bookmark = Bookmark.from(oidDto);
+            final RootOid rootOid = RootOid.create(bookmark);
+            return super.adapterFor(rootOid);
+        } else {
+            return super.adapterFor(targetObject);
+        }
+    }
+
+
 
     @javax.inject.Inject
     private BackgroundCommandServiceJdoRepository backgroundCommandRepository;
