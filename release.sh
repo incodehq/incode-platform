bf77b48284cfba329a2b28907bb0907bd2a5ab1e
Dan Haywood <dan@haywood-associates.co.uk>
2016-12-01 16:02:18 +0000

    adds empty module-dom, pom.xml parented by incode-parent with release profile to the pom, copies over release.sh and README from other isisaddons module

---
+++ release.sh
@@ -0,0 +1,69 @@
+RELEASE_VERSION=$1
+shift
+SNAPSHOT_VERSION=$1
+shift
+KEYID=$1
+shift
+PASSPHRASE=$*
+
+if [ ! "$RELEASE_VERSION" -o ! "$SNAPSHOT_VERSION" -o ! "$KEYID" -o ! "$PASSPHRASE" ]; then
+    echo "usage: $(basename $0) [release_version] [snapshot_version] [keyid] [passphrase]" >&2
+    exit 1
+fi
+
+
+echo ""
+echo "sanity check (mvn clean package -o)"
+echo ""
+mvn clean package -o >/dev/null
+if [ $? != 0 ]; then
+    echo "... failed" >&2
+    exit 1
+fi
+
+
+echo ""
+echo "bumping version to $RELEASE_VERSION"
+echo ""
+sh ./bumpver.sh $RELEASE_VERSION
+if [ $? != 0 ]; then
+    echo "... failed" >&2
+    exit 1
+fi
+
+
+echo ""
+echo "double-check (mvn clean package -o)"
+echo ""
+mvn clean package -o >/dev/null
+if [ $? != 0 ]; then
+    echo "... failed" >&2
+    exit 1
+fi
+
+
+echo ""
+echo "releasing 'mixin' module (mvn clean deploy)"
+echo ""
+mvn clean deploy -Pdanhaywood-mavenmixin-sonatyperelease -Dpgp.secretkey=keyring:id=$KEYID -Dpgp.passphrase="literal:$PASSPHRASE"
+if [ $? != 0 ]; then
+    echo "... failed" >&2
+    exit 1
+fi
+
+
+
+echo ""
+echo "bumping version to $SNAPSHOT_VERSION"
+echo ""
+sh ./bumpver.sh $SNAPSHOT_VERSION
+if [ $? != 0 ]; then
+    echo "... failed" >&2
+    exit 1
+fi
+
+echo ""
+echo "now run:"
+echo ""
+echo "git push origin master && git push origin $RELEASE_VERSION"
+echo ""
