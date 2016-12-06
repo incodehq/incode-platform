122d20215254f14cf5dc07cf8b7eda9174456f3d
Dan Haywood <dan@haywood-associates.co.uk>
2016-12-06 20:47:19 +0000

    adds various new features leveraging flyway; updates docs; extends demo app for multiple vendors
    new features:
    - prevent migration from being run more than once
    - support "flyway.xxx" properties (for outOfOrder, schemas, baselineOnMigrate)
    - force DataNucleus to visit all persisted entities (to validate them eagerly)
    
    in demo app:
    - for multiple vendors
    - also demonstrate use of beforeMigrate.sql
    
    in README:
    - docs on above features
    - guidance on "flyway.schemas" property

--- module-dom/src/main/java/org/isisaddons/module/flywaydb/dom/FlywayJdoPersistenceManagerFactory.java
+++ module-dom/src/main/java/org/isisaddons/module/flywaydb/dom/FlywayJdoPersistenceManagerFactory.java
@@ -16,13 +16,30 @@
  */
 package org.isisaddons.module.flywaydb.dom;
 
+import java.sql.Connection;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
+
+import javax.jdo.Extent;
+import javax.jdo.PersistenceManager;
+
+import com.google.common.base.Function;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.Lists;
 
 import org.datanucleus.PropertyNames;
+import org.datanucleus.api.jdo.JDOPersistenceManager;
 import org.datanucleus.api.jdo.JDOPersistenceManagerFactory;
+import org.datanucleus.metadata.FileMetaData;
+import org.datanucleus.metadata.MetaDataManager;
 import org.datanucleus.metadata.PersistenceUnitMetaData;
 import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.callback.BaseFlywayCallback;
+
+import org.apache.isis.applib.AppManifest;
 
 /**
  * Can be disabled either in <code>persistor_datanucleus.properties</code> or by setting system property (-D).
@@ -43,30 +60,49 @@ import org.flywaydb.core.Flyway;
  */
 public class FlywayJdoPersistenceManagerFactory extends JDOPersistenceManagerFactory {
 
+    /**
+     * the PMF is instantiated twice by DataNucleusApplicationComponents; we only want to do the migration once.
+     */
+    private static ThreadLocal<Boolean> whetherMigrationRun = new ThreadLocal() { { set(false); } };
+
     public FlywayJdoPersistenceManagerFactory() {
         super();
     }
 
     public FlywayJdoPersistenceManagerFactory(final PersistenceUnitMetaData pumd, final Map overrideProps) {
         super(pumd, overrideProps);
-        final boolean disabled = isDisabled(overrideProps);
-        if(!disabled) {
-            migrateDatabase(overrideProps);
-        }
+        migrateIfRequired(overrideProps);
     }
 
     public FlywayJdoPersistenceManagerFactory(final Map props) {
         super(props);
+        migrateIfRequired(props);
+    }
+
+    private void migrateIfRequired(final Map props) {
+        // don't migrate if disabled
         final boolean disabled = isDisabled(props);
-        if(!disabled) {
-            migrateDatabase(props);
+        if (disabled) {
+            return;
+        }
+
+        // don't migrate if already run migration (PMF gets instantiated twice during Isis' bootstrapping in DNAC)
+        if (whetherMigrationRun.get()) {
+            return;
+        }
+
+        // ok, go ahead
+        try {
+            migrateDatabaseIfNotAlready(props);
+        } finally {
+            whetherMigrationRun.set(true);
         }
     }
 
     /**
      * implicitly disabled if either of these DN flags set
      *
-     * Note though that we *don't* disable even if PropertyNames.PROPERTY_SCHEMA_AUTOCREATE_CONSTRAINTS is set.
+     * Note though that we *don't* disable if PropertyNames.PROPERTY_SCHEMA_AUTOCREATE_CONSTRAINTS is set.
      * This enables the use case where the DBA prefers to drop all constraints using a 'beforeMigrate' callback,
      * and then relies on DN to automatically create them afterwards.
      */
@@ -84,15 +120,19 @@ public class FlywayJdoPersistenceManagerFactory extends JDOPersistenceManagerFac
         }
     }
 
-    private void migrateDatabase(final Map map) {
-
+    private void migrateDatabaseIfNotAlready(final Map map) {
         try {
             Flyway flyway = new Flyway();
+
             final Properties properties = asProperties(map);
             flyway.configure(properties);
             flyway.setDataSource(this.getConnectionURL(), this.getConnectionUserName(), this.getConnectionPassword());
+
+            flyway.setCallbacks(new TouchAllEntities());
+
             flyway.repair();
             flyway.migrate();
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -100,10 +140,10 @@ public class FlywayJdoPersistenceManagerFactory extends JDOPersistenceManagerFac
 
     private static Properties asProperties(final Map map) {
         final Properties properties = new Properties();
-        for (Object key : properties.keySet()) {
+        for (Object key : map.keySet()) {
             if(key instanceof String) {
                 final String keyStr = (String) key;
-                if(((String) key).startsWith("flyway.")) {
+                if((keyStr).startsWith("flyway.")) {
                     final Object value = map.get(keyStr);
                     if(value instanceof String) {
                         final String valueStr = (String) value;
@@ -113,5 +153,59 @@ public class FlywayJdoPersistenceManagerFactory extends JDOPersistenceManagerFac
             }
         }
         return properties;
+    }
+
+    /**
+     * Ensures that all entities are visited and validated (and if autoCreateConstraints=true, then have their constraints created also).
+     */
+    private class TouchAllEntities extends BaseFlywayCallback {
+        @Override
+        public void afterMigrate(final Connection connection) {
+
+            PersistenceManager persistenceManager = null;
+            try {
+                persistenceManager = getPersistenceManager();
+                final Set<Class<?>> persistenceCapableTypes = AppManifest.Registry.instance().getPersistenceCapableTypes();
+
+                // this doesn't seem to be enough to trigger validation
+                // loadClasses(persistenceManager, persistenceCapableTypes);
+
+                // but this does
+                for (Class<?> persistenceCapableType : persistenceCapableTypes) {
+                    final Extent<?> unused = persistenceManager.getExtent(persistenceCapableType);
+                }
+            } finally {
+                closeQuietly(persistenceManager);
+            }
+        }
+
+        private void loadClasses(
+                final PersistenceManager persistenceManager,
+                final Set<Class<?>> persistenceCapableTypes1) {
+            JDOPersistenceManager jdoPm = (JDOPersistenceManager) persistenceManager;
+            final MetaDataManager metaDataManager = jdoPm.getExecutionContext().getMetaDataManager();
+            List<String> classNameList = Lists.newArrayList(FluentIterable.from(persistenceCapableTypes1).transform(
+                    new Function<Class<?>, String>() {
+                        @Override
+                        public String apply(final Class<?> input) {
+                            return input.getName();
+                        }
+                    }));
+            final String[] classNameArray = classNameList.toArray(new String[] {});
+            final Collection<String> classesWithMetaData = metaDataManager.getClassesWithMetaData();
+            System.out.println(classesWithMetaData.toArray());
+            final FileMetaData[] fileMetaDatas = metaDataManager.loadClasses(classNameArray, null);
+        }
+
+        private void closeQuietly(final PersistenceManager persistenceManager) {
+            if(persistenceManager != null) {
+                try {
+                    persistenceManager.close();
+                } catch(Exception ignore) {
+                    // ignore
+                }
+
+            }
+        }
     }
 }
