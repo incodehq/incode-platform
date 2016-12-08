9f186e57f36fb6c6fa8f1d8bb16d8728a50be59f
Dan Haywood <dan@haywood-associates.co.uk>
2016-12-08 12:21:38 +0000

    improves delta.sh

--- util/scripts/delta.sh
+++ util/scripts/delta.sh
@@ -4,20 +4,38 @@ PROD_URL=$1
 DEV_URL=$2
 USERNAME=$3
 PASSWORD=$4
+DIFFTYPES=$5
+SCHEMAS=$6
 
 if [ "$PROD_URL" == "" -o "$DEV_URL" == "" -o "$USERNAME" == "" -o "$PASSWORD" == "" ]; then
-    echo "usage: full.sh prod_url dev_url username password" >&2
+    echo "usage: full.sh prod_url dev_url username password [difftypes]" >&2
     exit 1
 fi
 
 CLASSPATH=$HOME/.m2/repository/com/microsoft/sqlserver/sqljdbc4/4.2-6225/sqljdbc4-4.2-6225.jar
 DRIVER=com.microsoft.sqlserver.jdbc.SQLServerDriver
 
+if [ "$DIFFTYPES" == "" ]; then
+
+    DIFFTYPES="tables,columns,indexes,foreignkeys,primarykeys"
+
+
+    # these also are valid (though not every combination is)
+
+    #DIFFTYPES="tables,columns,indexes,foreignkeys,primarykeys,uniqueconstraints"
+    #DIFFTYPES="tables,views,columns,indexes,foreignkeys,primarykeys"
+    #DIFFTYPES="tables,views,columns,indexes,foreignkeys,primarykeys,uniqueconstraints"
+    #DIFFTYPES="tables"
+    #DIFFTYPES="views"
+fi
+
+if [ "$SCHEMAS" == "" ]; then
+    SCHEMAS=`cat schema.txt`
+fi
 
 TMPFILE=/tmp/db.changelog.$$.xml
 
-
-for SCHEMA in `cat schema.txt`
+for SCHEMA in $SCHEMAS
 do
 
     echo "" >&2
@@ -34,13 +52,13 @@ do
 
     echo "" >&2
     echo "running diffChangeLog to determine differences between prod and dev ..."    >&2
-    echo "('views' are excluded from diffTypes) ..."    >&2
+    echo "(diffTypes = $DIFFTYPES) ..."    >&2
     liquibase --classpath=$CLASSPATH \
             --driver=$DRIVER \
             --url=$PROD_URL \
             --username=$USERNAME \
             --password=$PASSWORD \
-            --diffTypes="tables,columns,indexes,foreignkeys,primarykeys,uniqueconstraints" \
+            --diffTypes=$DIFFTYPES \
         diffChangeLog \
             --referenceUrl=$DEV_URL \
             --referenceUsername=$USERNAME \
