b7b7492b6b0db7ea2d6f6574c5d85a33089efeed
Dan Haywood <dan@haywood-associates.co.uk>
2017-04-12 23:21:27 +0100

    fixes #6

--- dom/src/main/java/org/isisaddons/module/audit/dom/Object_recentAuditEntries.java
+++ dom/src/main/java/org/isisaddons/module/audit/dom/Object_recentAuditEntries.java
@@ -10,14 +10,22 @@ import com.google.common.base.Predicates;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.Lists;
 
-import org.apache.isis.applib.annotation.*;
+import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.ActionLayout;
+import org.apache.isis.applib.annotation.Contributed;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.Mixin;
+import org.apache.isis.applib.annotation.ParameterLayout;
+import org.apache.isis.applib.annotation.SemanticsOf;
 import org.apache.isis.applib.services.HasTransactionId;
 import org.apache.isis.applib.services.appfeat.ApplicationFeatureRepository;
 import org.apache.isis.applib.services.appfeat.ApplicationMemberType;
 import org.apache.isis.applib.services.bookmark.Bookmark;
 import org.apache.isis.applib.services.bookmark.BookmarkService2;
+import org.apache.isis.applib.services.metamodel.MetaModelService2;
+import org.apache.isis.applib.services.metamodel.MetaModelService3;
 
-@Mixin
+@Mixin(method="act")
 public class Object_recentAuditEntries {
 
     private final Object domainObject;
@@ -38,13 +46,13 @@ public class Object_recentAuditEntries {
             contributed = Contributed.AS_ACTION
     )
     @MemberOrder(name = "Metadata", sequence = "10")
-    public List<AuditEntry> $$(
+    public List<AuditEntry> act(
             @ParameterLayout(named = "Object property")
             final String propertyName) {
         final Bookmark target = bookmarkService.bookmarkFor(domainObject);
         return auditingServiceRepository.findRecentByTargetAndPropertyId(target, propertyName);
     }
-    public List<String> choices0$$() {
+    public List<String> choices0Act() {
         final Class<?> domainClass = domainObject.getClass();
         final String packageName = domainClass.getPackage().getName();
         final String className = domainClass.getSimpleName();
@@ -54,13 +62,14 @@ public class Object_recentAuditEntries {
                                 .memberNamesOf(packageName, className, ApplicationMemberType.PROPERTY)
                 ).filter(Predicates.not(excludedProperties)));
     }
-    public String default0$$() {
-        final List<String> choices = choices0$$();
+    public String default0Act() {
+        final List<String> choices = choices0Act();
         return choices.size() == 1 ? choices.get(0): null;
     }
 
-    public boolean hide$$() {
-        return domainObject instanceof HasTransactionId;
+    public boolean hideAct() {
+        MetaModelService2.Sort sort = metaModelService3.sortOf(domainObject.getClass(), MetaModelService3.Mode.RELAXED);
+        return !(sort.isJdoEntity() || domainObject instanceof HasTransactionId);
     }
 
     static final Predicate<String> excludedProperties = new Predicate<String>() {
@@ -73,6 +82,8 @@ public class Object_recentAuditEntries {
         }
     };
 
+    @javax.inject.Inject
+    private MetaModelService3 metaModelService3;
 
     @Inject
     ApplicationFeatureRepository applicationFeatureRepository;
