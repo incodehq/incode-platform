a6a66a3e12046ac814ee644963f4c13b67119b59
Dan Haywood <dan@haywood-associates.co.uk>
2017-06-04 15:33:44 +0100

    moves isis-module-stringinterpolator to own directory

---
+++ isis-module-stringinterpolator/dom/src/main/java/org/isisaddons/module/stringinterpolator/dom/StringInterpolatorHelper.java
@@ -0,0 +1,94 @@
+/*
+ *  Copyright 2014 Dan Haywood
+ *
+ *  Licensed under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.isisaddons.module.stringinterpolator.dom;
+
+import ognl.Ognl;
+import ognl.OgnlException;
+
+class StringInterpolatorHelper {
+
+    private final String template;
+    private final boolean strict;
+    private StringInterpolatorService.Root root;
+
+    StringInterpolatorHelper(final String template, final StringInterpolatorService.Root root, final boolean strict) {
+        this.template = template;
+        this.root = root;
+        this.strict = strict;
+    }
+
+    String interpolate() {
+
+        final StringBuilder buffer = new StringBuilder();
+
+        int start;
+        int pos = 0;
+
+        while ((start = template.indexOf("${", pos)) != -1) {
+
+            // Append text before possible expression
+            buffer.append(template.substring(pos, start));
+
+            // Position is now where we found the "${"
+            pos = start;
+
+            // Get start and end of expression
+            final int startExpressionPos = start + 2;
+            final int endExpressionPos = template.indexOf('}', startExpressionPos);
+            if (endExpressionPos == -1) {
+                break;
+            }
+
+            pos = interpolate(buffer, startExpressionPos, endExpressionPos);
+        }
+
+        appendRemainder(buffer, pos);
+
+        return buffer.toString();
+    }
+
+    private int interpolate(final StringBuilder buffer, final int startExpressionPos, final int endExpressionPos) {
+        final String expression = template.substring(startExpressionPos, endExpressionPos);
+        final String str = evaluate(expression);
+        buffer.append(str);
+        return endExpressionPos + 1;
+    }
+
+    private String evaluate(final String expression) {
+        final String phraseValue = evaluateOgnl(expression);
+        return phraseValue != null ? phraseValue : "${" + expression + "}";
+    }
+
+    private String evaluateOgnl(final String expression) {
+        try {
+            Object value = Ognl.getValue(expression, root);
+            return value != null ? value.toString() : "";
+        } catch (OgnlException ex) {
+            if (strict) {
+                throw new RuntimeException("could not parse: " + expression, ex);
+            }
+            return null;
+        }
+    }
+
+    private void appendRemainder(final StringBuilder buffer, final int pos) {
+        if (pos < template.length()) {
+            buffer.append(template.substring(pos));
+        }
+    }
+}
+
---
+++ isis-module-stringinterpolator/integtests/src/test/java/org/isisaddons/module/stringinterpolator/integtests/demo/StringInterpolatorDemoToDoItemReportingContributionsTest.java
@@ -0,0 +1,70 @@
+/*
+ *  Copyright 2014 Dan Haywood
+ *
+ *  Licensed under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.isisaddons.module.stringinterpolator.integtests.demo;
+
+import java.net.URL;
+import javax.inject.Inject;
+
+import org.apache.isis.core.commons.config.IsisConfiguration;
+import org.isisaddons.module.stringinterpolator.fixture.dom.StringInterpolatorDemoToDoItem;
+import org.isisaddons.module.stringinterpolator.fixture.dom.StringInterpolatorDemoToDoItemReportingContributions;
+import org.isisaddons.module.stringinterpolator.fixture.dom.StringInterpolatorDemoToDoItems;
+import org.isisaddons.module.stringinterpolator.fixture.scripts.StringInterpolatorDemoToDoItemsFixture;
+import org.isisaddons.module.stringinterpolator.integtests.StringInterpolatorDemoIntegTest;
+import org.junit.Before;
+import org.junit.Test;
+import org.apache.isis.core.runtime.system.context.IsisContext;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class StringInterpolatorDemoToDoItemReportingContributionsTest extends StringInterpolatorDemoIntegTest {
+
+    @Before
+    public void setUpData() throws Exception {
+        scenarioExecution().install(new StringInterpolatorDemoToDoItemsFixture());
+    }
+
+    @Inject
+    StringInterpolatorDemoToDoItems toDoItems;
+
+    @Inject
+    IsisConfiguration configuration;
+
+    @Inject
+    StringInterpolatorDemoToDoItemReportingContributions toDoItemReportingContributions;
+
+    public static class Open extends StringInterpolatorDemoToDoItemReportingContributionsTest {
+
+        @Test
+        public void happyCase() throws Exception {
+
+            // given
+            assertThat(configuration.getString("isis.website"), is("http://isis.apache.org"));
+            assertThat(toDoItemReportingContributions.TEMPLATE, is("${properties['isis.website']}/${this.documentationPage}"));
+
+            final StringInterpolatorDemoToDoItem toDoItem = toDoItems.allToDos().get(0);
+            assertThat(toDoItem.getDocumentationPage(), is("documentation.html"));
+
+            // when
+            final URL url = toDoItemReportingContributions.open(toDoItem);
+
+            // then
+            assertThat(url.toExternalForm(), is("http://isis.apache.org/documentation.html"));
+        }
+    }
+}
