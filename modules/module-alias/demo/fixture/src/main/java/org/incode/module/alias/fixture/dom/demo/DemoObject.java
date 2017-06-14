96fd0494aad384cc35d797f20e9e239fe9cf8328
Dan Haywood <dan@haywood-associates.co.uk>
2017-06-14 08:45:50 +0100

    removes stale/invalid copyright notices from source files

--- modules/module-alias/demo/fixture/src/main/java/org/incode/module/alias/fixture/dom/demo/DemoObject.java
+++ modules/module-alias/demo/fixture/src/main/java/org/incode/module/alias/fixture/dom/demo/DemoObject.java
@@ -1,26 +1,14 @@
-/*
- *  Copyright 2014 Dan Haywood
- *
- *  Licensed under the Apache License, Version 2.0 (the
- *  "License"); you may not use this file except in compliance
- *  with the License.  You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
- */
 package org.incode.module.alias.fixture.dom.demo;
-
-import org.apache.isis.applib.annotation.*;
-import org.apache.isis.applib.util.ObjectContracts;
 
 import javax.jdo.annotations.IdentityType;
 import javax.jdo.annotations.VersionStrategy;
+
+import org.apache.isis.applib.annotation.BookmarkPolicy;
+import org.apache.isis.applib.annotation.DomainObject;
+import org.apache.isis.applib.annotation.DomainObjectLayout;
+import org.apache.isis.applib.annotation.Editing;
+import org.apache.isis.applib.annotation.Title;
+import org.apache.isis.applib.util.ObjectContracts;
 
 @javax.jdo.annotations.PersistenceCapable(
         identityType=IdentityType.DATASTORE,
