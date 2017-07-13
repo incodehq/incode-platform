4a778d45ac1db4e66ec53e972becd8c336f9f844
Dan Haywood <dan@haywood-associates.co.uk>
2017-07-13 22:51:28 +0100

    moves FixtureSpecProviders back to respective app submodules for each module

--- modules/module-classification/demo/app/src/main/java/org/incode/module/classification/fixture/scripts/ClassificationDemoObjectsFixtureScriptsSpecificationProvider.java
+++ modules/module-classification/demo/app/src/main/java/org/incode/module/classification/fixture/scripts/ClassificationDemoObjectsFixtureScriptsSpecificationProvider.java
@@ -5,6 +5,7 @@ import org.apache.isis.applib.annotation.NatureOfService;
 import org.apache.isis.applib.fixturescripts.FixtureScripts;
 import org.apache.isis.applib.services.fixturespec.FixtureScriptsSpecification;
 import org.apache.isis.applib.services.fixturespec.FixtureScriptsSpecificationProvider;
+
 import org.incode.module.classification.fixture.scripts.scenarios.ClassifiedDemoObjectsFixture;
 
 /**
