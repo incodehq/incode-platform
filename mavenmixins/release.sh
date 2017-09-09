20cbfd9b4cd6f0a356a0763badb102dbfe0d984b
Dan Haywood <dan@haywood-associates.co.uk>
2017-09-10 00:14:31 +0100

    working on release procedures

---
+++ mavenmixins/release.sh
@@ -0,0 +1,81 @@
+RELEASE_VERSION=$1
+shift
+SNAPSHOT_VERSION=$1
+shift
+KEYID=$1
+shift
+PASSPHRASE=$*
+
+if [ ! "$RELEASE_VERSION" -o ! "$SNAPSHOT_VERSION" -o ! "$KEYID" -o ! "$PASSPHRASE" ]; then
+    echo "usage: $(basename $0) [release_version] [snapshot_version] [keyid] [passphrase]" >&2
+    exit 1
+fi
+
+
+echo ""
+echo "checking no reference to isis.version of -SNAPSHOT"
+echo ""
+grep SNAPSHOT pom.xml | grep isis.version
+if [ $? == 0 ]; then
+    echo ""
+    echo "... failed" >&2
+    exit 1
+fi
+
+
+echo ""
+echo "sanity check (mvn clean install -T1C -o -Dskip.isis-swagger  -Dskip.isis-validate) "
+echo ""
+mvn clean install -T1C -Dskip.isis-swagger  -Dskip.isis-validate -o >/dev/null
+if [ $? != 0 ]; then
+    echo "... failed" >&2
+    exit 1
+fi
+
+
+echo ""
+echo "bumping version to $RELEASE_VERSION"
+echo ""
+sh ../scripts/bumpver.sh $RELEASE_VERSION
+if [ $? != 0 ]; then
+    echo "... failed" >&2
+    exit 1
+fi
+
+
+echo ""
+echo "double-check (mvn clean install -T1C -o -Dskip.isis-swagger  -Dskip.isis-validate)"
+echo ""
+mvn clean install -T1C -Dskip.isis-swagger  -Dskip.isis-validate -o >/dev/null
+if [ $? != 0 ]; then
+    echo "... failed" >&2
+    exit 1
+fi
+
+
+echo ""
+echo "releasing (mvn clean deploy -P release)"
+echo ""
+mvn clean deploy -P sonatyperelease -Dskip.isis-swagger  -Dskip.isis-validate -Dpgp.secretkey=keyring:id=$KEYID -Dpgp.passphrase="literal:$PASSPHRASE"
+if [ $? != 0 ]; then
+    echo "... failed" >&2
+    exit 1
+fi
+
+
+
+echo ""
+echo "bumping version to $SNAPSHOT_VERSION"
+echo ""
+sh ../scripts/bumpver.sh $SNAPSHOT_VERSION
+if [ $? != 0 ]; then
+    echo "... failed" >&2
+    exit 1
+fi
+
+
+echo ""
+echo "now run:"
+echo ""
+echo "git push origin master && git push origin $RELEASE_VERSION"
+echo ""
