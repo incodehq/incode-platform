34dc89e8bb142e4ea0f5e0847a1192474b291e0f
Dan Haywood <dan@haywood-associates.co.uk>
2017-09-10 08:21:25 +0100

    build scripts

--- adocs/documentation/publish.sh
+++ adocs/documentation/publish.sh
@@ -4,7 +4,8 @@ if [ "$1" = "" ]; then
 else
 	MSG=$1
 fi
+SHA_ID=`git rev-parse --verify HEAD`
 echo $MSG
 mvn clean 
-mvn install -f pom-pdf.xml -D message="$MSG"
-mvn install                -D message="$MSG"
+mvn install -f pom-pdf.xml
+mvn install -D message="$MSG (commit: $SHA_ID)"
