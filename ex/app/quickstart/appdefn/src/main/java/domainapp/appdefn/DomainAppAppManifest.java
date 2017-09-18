76771e05d47fec707765be86d9126c1e08a9ec33
Dan Haywood <dan@haywood-associates.co.uk>
2017-09-18 15:26:17 +0100

    fixes for example app (optional modules referenced in the example app manifest); improves release docs

--- ex/app/quickstart/appdefn/src/main/java/domainapp/appdefn/DomainAppAppManifest.java
+++ ex/app/quickstart/appdefn/src/main/java/domainapp/appdefn/DomainAppAppManifest.java
@@ -3,19 +3,9 @@ package domainapp.appdefn;
 import org.apache.isis.applib.AppManifestAbstract;
 
 import org.isisaddons.module.audit.AuditModule;
-import org.isisaddons.module.docx.DocxModule;
-import org.isisaddons.module.excel.ExcelModule;
-import org.isisaddons.module.fakedata.FakeDataModule;
-import org.isisaddons.module.freemarker.dom.FreeMarkerModule;
-import org.isisaddons.module.pdfbox.dom.PdfBoxModule;
-import org.isisaddons.module.poly.PolyModule;
 import org.isisaddons.module.publishmq.PublishMqModule;
-import org.isisaddons.module.servletapi.ServletApiModule;
 import org.isisaddons.module.sessionlogger.SessionLoggerModule;
-import org.isisaddons.module.stringinterpolator.StringInterpolatorModule;
-import org.isisaddons.module.tags.TagsModule;
 import org.isisaddons.module.togglz.TogglzModule;
-import org.isisaddons.module.xdocreport.dom.XDocReportModule;
 import org.isisaddons.wicket.excel.cpt.ui.ExcelUiModule;
 import org.isisaddons.wicket.fullcalendar2.cpt.ui.FullCalendar2UiModule;
 import org.isisaddons.wicket.gmap3.cpt.applib.Gmap3ApplibModule;
@@ -24,18 +14,7 @@ import org.isisaddons.wicket.gmap3.cpt.ui.Gmap3UiModule;
 import org.isisaddons.wicket.pdfjs.cpt.PdfjsCptModule;
 import org.isisaddons.wicket.wickedcharts.cpt.ui.WickedChartsUiModule;
 
-import org.incode.module.alias.dom.AliasModule;
 import org.incode.module.base.services.BaseServicesModule;
-import org.incode.module.classification.dom.ClassificationModule;
-import org.incode.module.commchannel.dom.CommChannelModule;
-import org.incode.module.communications.dom.CommunicationsModuleDomModule;
-import org.incode.module.country.dom.CountryModule;
-import org.incode.module.docfragment.dom.DocFragmentModuleDomModule;
-import org.incode.module.docrendering.freemarker.dom.FreemarkerDocRenderingModule;
-import org.incode.module.docrendering.stringinterpolator.dom.StringInterpolatorDocRenderingModule;
-import org.incode.module.docrendering.xdocreport.dom.XDocReportDocRenderingModule;
-import org.incode.module.document.dom.DocumentModule;
-import org.incode.module.note.dom.NoteModule;
 
 import domainapp.modules.simple.SimpleModule;
 
@@ -50,35 +29,6 @@ public class DomainAppAppManifest extends AppManifestAbstract {
             /* Uncomment to include example modules that set up embedded camel: START */
             domainapp.example.embeddedcamel.EmbeddedCamelModule.class,
             /* Uncomment to include example modules that set up embedded camel: END */
-
-            /* Uncomment to include example modules demonstrating platform usage: START */
-
-            // lib
-            FreemarkerDocRenderingModule.class,
-            StringInterpolatorDocRenderingModule.class,
-            XDocReportDocRenderingModule.class,
-            DocxModule.class,
-            ExcelModule.class,
-            FakeDataModule.class,
-            FreeMarkerModule.class,
-            PolyModule.class,
-            PdfBoxModule.class,
-            ServletApiModule.class,
-            StringInterpolatorModule.class,
-            XDocReportModule.class,
-
-            // subdomains (dom)
-            AliasModule.class,
-            ClassificationModule.class,
-            CommChannelModule.class,
-            CommunicationsModuleDomModule.class,
-            CountryModule.class,
-            DocFragmentModuleDomModule.class,
-            DocumentModule.class,
-            NoteModule.class,
-            TagsModule.class,
-
-            /* Uncomment to include example modules demonstrating platform usage: END */
 
             // extensions
             TogglzModule.class,
--- ex/app/quickstart/example/appdefn/src/main/java/org/incode/domainapp/example/app/DomainAppAppManifestWithExampleModules.java
+++ ex/app/quickstart/example/appdefn/src/main/java/org/incode/domainapp/example/app/DomainAppAppManifestWithExampleModules.java
@@ -2,8 +2,30 @@ package org.incode.domainapp.example.app;
 
 import org.apache.isis.applib.AppManifestAbstract;
 
+import org.isisaddons.module.docx.DocxModule;
+import org.isisaddons.module.excel.ExcelModule;
+import org.isisaddons.module.fakedata.FakeDataModule;
+import org.isisaddons.module.freemarker.dom.FreeMarkerModule;
+import org.isisaddons.module.pdfbox.dom.PdfBoxModule;
+import org.isisaddons.module.poly.PolyModule;
+import org.isisaddons.module.servletapi.ServletApiModule;
+import org.isisaddons.module.stringinterpolator.StringInterpolatorModule;
+import org.isisaddons.module.tags.TagsModule;
+import org.isisaddons.module.xdocreport.dom.XDocReportModule;
+
 import org.incode.domainapp.example.dom.ExampleDomSubmodule;
 import org.incode.domainapp.example.dom.demo.ExampleDomDemoDomSubmodule;
+import org.incode.module.alias.dom.AliasModule;
+import org.incode.module.classification.dom.ClassificationModule;
+import org.incode.module.commchannel.dom.CommChannelModule;
+import org.incode.module.communications.dom.CommunicationsModuleDomModule;
+import org.incode.module.country.dom.CountryModule;
+import org.incode.module.docfragment.dom.DocFragmentModuleDomModule;
+import org.incode.module.docrendering.freemarker.dom.FreemarkerDocRenderingModule;
+import org.incode.module.docrendering.stringinterpolator.dom.StringInterpolatorDocRenderingModule;
+import org.incode.module.docrendering.xdocreport.dom.XDocReportDocRenderingModule;
+import org.incode.module.document.dom.DocumentModule;
+import org.incode.module.note.dom.NoteModule;
 
 import domainapp.appdefn.DomainAppAppManifest;
 
@@ -13,7 +35,33 @@ public class DomainAppAppManifestWithExampleModules extends AppManifestAbstract 
             .withAdditionalModules(
                     ExampleAppSubmodule.class,
                     ExampleDomSubmodule.class,
-                    ExampleDomDemoDomSubmodule.class
+                    ExampleDomDemoDomSubmodule.class,
+
+                    // lib
+                    FreemarkerDocRenderingModule.class,
+                    StringInterpolatorDocRenderingModule.class,
+                    XDocReportDocRenderingModule.class,
+                    DocxModule.class,
+                    ExcelModule.class,
+                    FakeDataModule.class,
+                    FreeMarkerModule.class,
+                    PolyModule.class,
+                    PdfBoxModule.class,
+                    ServletApiModule.class,
+                    StringInterpolatorModule.class,
+                    XDocReportModule.class,
+
+                    // subdomains (dom)
+                    AliasModule.class,
+                    ClassificationModule.class,
+                    CommChannelModule.class,
+                    CommunicationsModuleDomModule.class,
+                    CountryModule.class,
+                    DocFragmentModuleDomModule.class,
+                    DocumentModule.class,
+                    NoteModule.class,
+                    TagsModule.class
+
             );
 
     public DomainAppAppManifestWithExampleModules() {
