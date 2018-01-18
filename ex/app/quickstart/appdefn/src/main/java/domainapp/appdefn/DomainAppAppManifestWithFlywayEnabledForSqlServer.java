7ef834a71233a3e62f0318b2dc568b4e3979c22e
Dan Haywood <dan@haywood-associates.co.uk>
2018-01-18 15:24:12 +0000

    simplifies the DomainApp manifests for quickstart example app.

--- ex/app/quickstart/appdefn/src/main/java/domainapp/appdefn/DomainAppAppManifestWithFlywayEnabledForSqlServer.java
+++ ex/app/quickstart/appdefn/src/main/java/domainapp/appdefn/DomainAppAppManifestWithFlywayEnabledForSqlServer.java
@@ -1,25 +1,15 @@
 package domainapp.appdefn;
 
-import java.util.List;
-
 import org.apache.isis.applib.AppManifestAbstract2;
-import org.apache.isis.applib.fixturescripts.FixtureScript;
 
-import domainapp.appdefn.seed.security.SeedSuperAdministratorRoleAndSvenSuperUser;
 public class DomainAppAppManifestWithFlywayEnabledForSqlServer extends AppManifestAbstract2 {
 
 	public DomainAppAppManifestWithFlywayEnabledForSqlServer() {
 		super(DomainAppAppManifest.BUILDER
-		//.withFixtureScripts(SeedSuperAdministratorRoleAndSvenSuperUser.class)
 		.withConfigurationPropertiesFile(
                     DomainAppAppManifestWithFlywayEnabledForSqlServer.class,
-                    "persistor-sqlserver-rtrm_db.properties")
+                    "persistor-sqlserver-quickstart_db.properties")
 		);
 	}
 
-	@Override
-    protected void overrideFixtures(final List<Class<? extends FixtureScript>> fixtureScriptClasses) {
-        // using withFixtureScripts(...) is broken in 1.16.0
-        fixtureScriptClasses.add(SeedSuperAdministratorRoleAndSvenSuperUser.class);
-    }
 }
