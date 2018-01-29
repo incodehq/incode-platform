e969430e4d49c2ed8eb3a37e4a7fe3dbc96a726c
Dan Haywood <dan@haywood-associates.co.uk>
2018-01-29 23:40:01 +0000

    #69 - tidies up logging fo background execution subclasses

--- modules/spi/command/impl/src/main/java/org/isisaddons/module/command/dom/BackgroundCommandExecutionFromBackgroundCommandServiceJdo.java
+++ modules/spi/command/impl/src/main/java/org/isisaddons/module/command/dom/BackgroundCommandExecutionFromBackgroundCommandServiceJdo.java
@@ -2,24 +2,15 @@ package org.isisaddons.module.command.dom;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import org.apache.isis.applib.services.command.Command;
 import org.apache.isis.core.runtime.services.background.BackgroundCommandExecution;
 
 public class BackgroundCommandExecutionFromBackgroundCommandServiceJdo extends BackgroundCommandExecution {
 
-    @SuppressWarnings("unused")
-    private final static Logger LOG = LoggerFactory.getLogger(BackgroundCommandExecutionFromBackgroundCommandServiceJdo.class);
-
     @Override
     protected List<? extends Command> findBackgroundCommandsToExecute() {
-        final List<CommandJdo> commands = backgroundCommandRepository.findBackgroundCommandsNotYetStarted();
-        LOG.debug("Found {} to execute", commands.size());
-        return commands; 
+        return backgroundCommandRepository.findBackgroundCommandsNotYetStarted();
     }
-
 
     @javax.inject.Inject
     BackgroundCommandServiceJdoRepository backgroundCommandRepository;
--- modules/spi/command/replay/src/main/java/org/isisaddons/module/command/replay/impl/ReplayableCommandExecutionFromReplayableCommandServiceJdo.java
+++ modules/spi/command/replay/src/main/java/org/isisaddons/module/command/replay/impl/ReplayableCommandExecutionFromReplayableCommandServiceJdo.java
@@ -2,19 +2,12 @@ package org.isisaddons.module.command.replay.impl;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import org.apache.isis.applib.services.command.Command;
 import org.apache.isis.core.runtime.services.background.BackgroundCommandExecution;
 
-import org.isisaddons.module.command.dom.CommandJdo;
 import org.isisaddons.module.command.dom.ReplayableCommandServiceJdoRepository;
 
 public class ReplayableCommandExecutionFromReplayableCommandServiceJdo extends BackgroundCommandExecution {
-
-    @SuppressWarnings("unused")
-    private final static Logger LOG = LoggerFactory.getLogger(ReplayableCommandExecutionFromReplayableCommandServiceJdo.class);
 
     public ReplayableCommandExecutionFromReplayableCommandServiceJdo() {
         super(OnExceptionPolicy.QUIT);
@@ -22,11 +15,8 @@ public class ReplayableCommandExecutionFromReplayableCommandServiceJdo extends B
 
     @Override
     protected List<? extends Command> findBackgroundCommandsToExecute() {
-        final List<CommandJdo> commands = repository.findReplayableCommandsNotYetStartedUnlessBlocked();
-        LOG.debug("Found {} to execute", commands.size());
-        return commands; 
+        return repository.findReplayableCommandsNotYetStartedUnlessBlocked();
     }
-
 
     @javax.inject.Inject
     ReplayableCommandServiceJdoRepository repository;
