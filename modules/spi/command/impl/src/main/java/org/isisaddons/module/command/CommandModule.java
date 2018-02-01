3348d06dd797ed04f20308e7c200490120bb825e
Dan Haywood <dan@haywood-associates.co.uk>
2018-02-01 13:34:55 +0000

    #73 - updates all releasable module's pom.xml
    bumping to use incode-build v2.2 which references java-mavenmixin-standard
    0.0.4.
    
    adds in the new properties, and configures git-commit-id-plugin for all of
    these modules

--- modules/spi/command/impl/src/main/java/org/isisaddons/module/command/CommandModule.java
+++ modules/spi/command/impl/src/main/java/org/isisaddons/module/command/CommandModule.java
@@ -1,5 +1,14 @@
 package org.isisaddons.module.command;
 
+import org.isisaddons.module.command.dom.CommandDomModule;
+
+/**
+ * Note that referencing this module implicitly brings in the replay module if also on the class path.
+ *
+ * <p>
+ *     Alternatively, use {@link CommandDomModule} and <tt>CommandReplayModule</tt> separately.
+ * </p>
+ */
 public final class CommandModule {
 
     private CommandModule(){}
