93cc37932083546e00eed0d4c6188c0991dea30f
danhaywood <dan@haywood-associates.co.uk>
2018-09-28 15:56:47 +0200

    fixes build, revision set to 1.0.0-SNAPSHOT

--- enhance-all.sh
+++ enhance-all.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-mvn datanucleus:enhance@process-classes datanucleus:enhance@process-test-classes -Denhance -Dskip.default -o $*
+mvn datanucleus:enhance@process-classes datanucleus:enhance@process-test-classes -Denhance -Dskip.default  -Drevision=1.0.0-SNAPSHOT -o $*
 
