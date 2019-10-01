d32171e19b43ef1f15fa0f07e0083e9e395e32ae
danhaywood <dan@haywood-associates.co.uk>
2019-10-01 13:02:16 +0100

    #115 - adds query defns for OutboxEvent entity

--- modules/spi/publishmq/impl/jdo/src/main/java/org/isisaddons/module/publishmq/dom/jdo/events/OutboxEvent.java
+++ modules/spi/publishmq/impl/jdo/src/main/java/org/isisaddons/module/publishmq/dom/jdo/events/OutboxEvent.java
@@ -7,6 +7,20 @@ import org.apache.isis.applib.annotation.Editing;
 @javax.jdo.annotations.PersistenceCapable(
         schema = "isispublishmq",
         table="OutboxEvent")
+@javax.jdo.annotations.Queries( {
+        @javax.jdo.annotations.Query(
+                name="findByTransactionIdAndSequence", language="JDOQL",
+                value="SELECT "
+                        + "FROM org.isisaddons.module.publishmq.dom.jdo.events.OutboxEvent "
+                        + "WHERE transactionId == :transactionId "
+                        + "&&    sequence      == :sequence "),
+        @javax.jdo.annotations.Query(
+                name="findOldest", language="JDOQL",
+                value="SELECT "
+                        + "FROM org.isisaddons.module.publishmq.dom.jdo.events.OutboxEvent "
+                        + "ORDER BY timestamp ASC, transactionId ASC, sequence ASC "
+                        + "RANGE 0,30")
+})
 @DomainObject(
         editing = Editing.DISABLED,
         objectType = "isispublishmq.OutboxEvent"
